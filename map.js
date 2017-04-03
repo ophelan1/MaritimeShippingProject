745a679f5bad28fedb9ed6cd0e79c1a78655c067
hevans1 <hevans1@nd.edu>
2017-04-03 11:55:35 -0400

    Final changes for update 2. Map viz.

--- map.js
+++ map.js
@@ -4,13 +4,13 @@ function ExecuteMap() {
 
   var svg = d3.select( "body" )
     .append( "svg" )
-    .attr( "width", width )
-    .attr( "height", height );
+    .attr("width", width )
+    .attr("height", height );
 
   var g = svg.append( "g" );
 
   var proj = d3.geo.equirectangular()
-    .scale( 350 )
+    .scale( 300 )
     // .rotate( [71.057,0] )
     //.center( [0, 42.313] )
     .translate( [width/2,height/2] );
@@ -65,4 +65,27 @@ function ExecuteMap() {
         .attr("fill","#66cd00")
         .attr("stroke","#66cd00");
     });
+
+  // var slider = d3.select("#slider").call(d3.slider().axis(true).min(2006).max(2012).step(3));
+
+  var data = [
+      {name: "0-5", value: 14221},
+      {name: "6-10", value: 5641},
+      {name: "11-15", value: 2341},
+      {name: "16-20", value: 3568},
+      {name: "21-25", value: 1254},
+      {name: "26-30", value: 8795},
+      {name: "30+", value: 15227}
+  ];
+
+  var size = [
+      {name: "0-3K", value: 4122},
+      {name: "3K-6K", value: 5752},
+      {name: "6K-9K", value: 9852},
+      {name: "9K-12K", value: 7412},
+      {name: "12K-15K", value: 1021},
+  ];
+
+  d3.select("#age_filter").call(BarChart().width(400).height(150).labelPadding(1).data(data));
+  d3.select("#size_filter").call(BarChart().width(400).height(150).labelPadding(1).data(size));
 }
