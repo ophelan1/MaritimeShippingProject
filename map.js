7d74796d0ef4246931ffb95139b8853aa603cd9a
Holly Evans <Holly Evans>
2017-04-26 01:37:10 -0400

    Update traversals to only include routes with frequency >50. Add CheckBoxFilter and LineChart.

--- map.js
+++ map.js
@@ -1,309 +1,281 @@
-function ExecuteMap() {
-  var width = 2150,
-    height = 1000;
+function ExecuteMap(ports_dt, routes_dt, vessels_dt, traversals_dt) {
+    var ports_d = ports_dt;
+    var routes_d = routes_dt;
+    var vessels_d = vessels_dt;
+    var traversals_d = traversals_dt;
 
+    var width = 2150,
+        height = 1000;
 
-//######################################################################
-// Setup the map SVG
-  // USING GLOBAL VARIABLES FOR BOTH SVGs
-  mapSvg = d3.select("#mapContainer")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var mapSvg = d3.select("#mapContainer")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-  var g = mapSvg.append("g");
+    var g = mapSvg.append("g");
 
-  // Global proj variable
-  proj = d3.geo.equirectangular()
-    .scale(300)
-    // .rotate( [71.057,0] )
-    //.center( [0, 42.313] )
-    .translate([width/2,height/2]);
+    var proj = d3.geo.equirectangular()
+        .scale(300)
+        .translate([width/2,height/2]);
 
-  geoPath = d3.geo.path()
-      .projection(proj);
+    var geoPath = d3.geo.path()   
+        .projection(proj);
 
-  g.selectAll("path")
-    .data(worldgeo_json.features)
-    .enter()
-    .append("path")
-    .attr("fill", "#b7b7b7")
-    .attr("d", geoPath);
+    g.selectAll("path")
+        .data(worldgeo_json.features)
+        .enter()
+        .append("path")
+        .attr("fill", "#b7b7b7")
+        .attr("d", geoPath);
 
-//######################################################################
-// Setup the Bar Chart SVG
-  var margin = {top: 30, right: 20, bottom: 40, left: 80};
-
- // The X & Y Scale
-  var x = d3.scale.linear()
-      .range([0, width])
-  var y = d3.scale.linear().range([height, 0]);
-  var cScale = d3.scale.category20();
-
-  // THE X & Y Axis
-  var xAxis = d3.svg.axis().scale(x)
-      .orient("bottom")
-      .tickValues(['2006', '2009', '2012'])
-  var yAxis = d3.svg.axis().scale(y)
-      .orient("left").ticks(5);
-
-
-  valueline = d3.svg.line()
-      .x(function(d) { return x(d.year); })
-      .y(function(d) { return y(d.frequency); });
-
-  //Global SVG Variable
-  barSvg = d3.select("#chartContainer")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // Scale the range of the data
-  x.domain([2005, 2013]);
-  y.domain([0, 1000]);
-
-  // Add the X Axis
-  barSvg.append("g")     
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  // Add the Y Axis
-  barSvg.append("g")     
-      .attr("class", "y axis")
-      .call(yAxis);
-
-  lineArray = {};
-
-
-
-//######################################################################
-// Setup the Filter Section
   
   // var slider = d3.select("#slider").call(d3.slider().axis(true).min(2006).max(2012).step(3));
 
-  var data = [
-      {name: "0-5", value: 14221},
-      {name: "6-10", value: 5641},
-      {name: "11-15", value: 2341},
-      {name: "16-20", value: 3568},
-      {name: "21-25", value: 1254},
-      {name: "26-30", value: 8795},
-      {name: "30+", value: 15227}
-  ];
+    var age = [
+        {name: "0-5", func: function(datum, checked) {
+            return datum.age == "0-5" && checked == true;
+        }},
+        {name: "6-10", func: function(datum, checked) {
+            return datum.age == "6-10" && checked == true;
+        }},
+        {name: "11-15", func: function(datum, checked) {
+            return datum.age == "11-15" && checked == true;
+        }},
+        {name: "16-20", func: function(datum, checked) {
+            return datum.age == "16-20" && checked == true;
+        }},
+        {name: "21-25", func: function(datum, checked) {
+            return datum.age == "21-25" && checked == true;
+        }},
+        {name: "26-30", func: function(datum, checked) {
+            return datum.age == "26-30" && checked == true;
+        }},
+        {name: "30+", func: function(datum, checked) {
+            return datum.age == "31+" && checked == true;
+        }},
+    ];
 
-  var size = [
-      {name: "0-3K", value: 4122},
-      {name: "3K-6K", value: 5752},
-      {name: "6K-9K", value: 9852},
-      {name: "9K-12K", value: 7412},
-      {name: "12K-15K", value: 1021},
-  ];
+    var age_filter = CheckBoxFilter("age").data(age);
+    d3.select("#age_filter").call(age_filter);
 
-  d3.select("#age_filter").call(BarChart().width(400).height(150).labelPadding(1).data(data));
-  d3.select("#size_filter").call(BarChart().width(400).height(150).labelPadding(1).data(size));
+    var type = [
+        {name: "Bulk", func: function(datum, checked) {
+            return datum.type == "Bulk" && checked == true;
+        }},
+        {name: "Container", func: function(datum, checked) {
+            return datum.type == "Container" && checked == true;
+        }}
+    ];
 
+    var type_filter = CheckBoxFilter("type").data(type);
+    d3.select("#type_filter").call(type_filter);
 
+    var button = d3.select("div.button")
+        .append("button")
+        .attr("class","generate-button")
+        .text("Generate Routes")
+        .on("click", drawRoutes);
 
-    // var width_scale = d3.scale.linear()
-    //   .range([0,5]);
+    var linechart = LineChart();
+    d3.select("#chartContainer").call(linechart);
 
-    // width_scale.domain([
-    //   d3.min(routes_json.features, function(d) { return d.properties.frequency; }),
-    //   d3.max(routes_json.features, function(d) { return d.properties.frequency; })
-    // ]);
-  };
+    function drawRoutes() {
 
+        var routes = mapSvg.append("g");
 
-  function setRoutes() {
-    d3.queue()
-    .defer(d3.csv, "http://localhost/~Owen/MaritimeShippingProject/Ports.csv", function(d) {
-      return {
-        id: d.id,
-        name: d.name,
-        country: d.country,
-        lat: +d.lat,
-        long: +d.long
-      };
-    })
-    .defer(d3.csv, "Routes.csv", function(d) {
-      return {
-        startportID: d.startportID,
-        endportID: d.endportID
-      };
-    })
-    .defer(d3.csv, "Vessels.csv", function(d) {
-      return {
-        id: d.id,
-        flag: d.flag,
-        type: d.type,
-        size: d.size,
-        owner: d.owner,
-        operator: d.operator
-      };
-    })
-    .defer(d3.csv, "Traversals.csv", function(d) {
-      return {
-        year: d.year,
-        vesselID: d.vesselID,
-        startportID: d.startportID,
-        endportID: d.endportID,
-        startdate: new Date(d.startdate),
-        enddate: new Date(d.enddate)
-      };
-    })
-    .await(ready);
-  }
+        var route_data = allFilters(ports_d, routes_d, vessels_d, traversals_d);
 
-  function ready(error, ports_d, routes_d, vessels_d, traversals_d) {
-    if (error) throw error;
+        routes.selectAll("path")
+            .data(route_data)
+            // .filter(function(d) {
+            //     console.log(route_data)
+            //     console.log(route_data.routeID);
+            //     console.log(d.routeID);
+            //     console.log(route_data.routeID.indexOf(d.routeID));
+            //     console.log(route_data.routeID.indexOf(d.routeID) === -1);
+            //     // return route_data.routeID.indexOf(d.routeID) === -1;
+            //     return true;
+            // })
+            .enter()
+            .append("path")
+            .attr("class", "route unselected")
+            .attr("id", function(d) {
+                return d.routeID;
+            })
+            .attr("d", function(d) {
+                var startport = ports_d.filter(function(d2){
+                    return d2.id == d.startportID;
+                });
+                var endport = ports_d.filter(function(d2){
+                    return d2.id == d.endportID;
+                });
+                var coords = [
+                    [startport[0].long, startport[0].lat],
+                    [endport[0].long, endport[0].lat]
+                ];
+                return buildRoute(coords);
+            })
+            .attr("fill", "#000000")
+            .attr("stroke", "#000000")
+            .attr("stroke-width", 3)// function(d) {
+            // return width_scale(d);
+            // })
+            .on("click", function() {
+                d3.select(this)
+                .attr("fill","#66cd00")
+                .attr("stroke","#66cd00");
+            }); 
 
-    // var routeFreq = getFrequencies(routes_d);
-    // console.log(routeFreq);
-    // console.log(routeFreq[0]);
-    // console.log(routeFreq[1]);
+        var ports = mapSvg.append("g");
 
-    var routes = mapSvg.append("g");
+        ports.selectAll("path")
+            .data(ports_d)
+            .enter()
+            .append("path")
+            .attr("class","port")
+            .attr("fill", "#900")
+            .attr("stroke", "#999")
+            .attr("d", function(d) {
+                var feature = {
+                    "type":"Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [d.long, d.lat]
+                    }
+                };
+                return geoPath(feature);
+            })
+            .on("click", function() {
+                d3.select(this)
+                    .attr("fill", "#342a99")
+                    .attr("stroke", "#342a99");
+            });
 
-    routes.selectAll("path")
-      .data(routes_d)
-      .enter()
-      .append("path")
-      .filter(function(d) {
-        return d.startportID < 1000 & d.endportID < 1000;
-        // console.log(routeFreq[i]);
-        // return routeFreq[i]>2;
-        // return d.startportID == 1182 | d.startportID == 238;
-        // return d.startportID < 1000 & d.endportID < 500;
-      })
-      .attr("d", function(d) {
-        var startport = ports_d.filter(function(d2){
-          return d2.id == d.startportID;
-        });
-        var endport = ports_d.filter(function(d2){
-          return d2.id == d.endportID;
-        });
-        var coords = [
-          [startport[0].long, startport[0].lat],
-          [endport[0].long, endport[0].lat]
-        ];
-        return buildRoute(coords);
-      })
-      .attr("fill", "#000000")
-      .attr("stroke", "#000000")
-      .attr("stroke-width", 3)// function(d) {
-        // return width_scale(d);
-      // })
-      .on("click", function() {
-        if(d3.select(this).attr("fill") == "#000000" ){
-          d3.select(this)
-            .attr("fill","#66cd00")
-            .attr("stroke","#66cd00");
-            var idInfo = d3.select(this).datum();
-            var startPortID = idInfo.startportID;
-            var endPortID = idInfo.endportID;
-            console.log(idInfo);
-            console.log(startPortID);
-            console.log(endPortID);
+        // var width_scale = d3.scale.linear()
+        //   .range([0,5]);
 
-        $.ajax({
-          url: 'php/tableSetup.php',
-          type: 'POST',
-          data: {routeID1:startPortID, routeID2:endPortID},
-          success: function(data) {
-            console.log(data);
-            $.ajax({
-              url: 'php/getValues.php',
-              type: 'POST',
-              success: function(data) {
-                console.log(data);
-                var routeData = JSON.parse(data);
-                console.log(routeData);
-                routeLine = barSvg.append("path")
-                      .attr("class", "line")
-                      .attr("d", valueline(routeData));
-              }
-            }); // end inner ajax cal
-          }
-        }); // end outer ajax cal
+        // width_scale.domain([
+        //   d3.min(routes_json.features, function(d) { return d.properties.frequency; }),
+        //   d3.max(routes_json.features, function(d) { return d.properties.frequency; })
+        // ]);
+    };
 
+    function buildRoute(coords) {
+        // var west = false;
+        // if ()
+
+        // calculate east to west if start >105 and ends up on the west coast 
+        // how to define west coast of americas?(<70 if south america or <)
+
+        var x180 = proj([180,coords[0][1]])[0];
+        var xm180 = proj([-179.999,coords[0][1]])[0];
+        var vert = proj(coords[1])[1] - proj(coords[0])[1]; // y of start minus y of end
+        var horz1;
+        var horz2;
+        var slope;
+
+        var path= "M" + proj(coords[0])[0] + "," + proj(coords[0])[1];
+        if (coords[0][0]>105 & coords[1][0] < -70) {
+            horz1 = x180 - proj(coords[0])[0];
+            horz2 = proj(coords[1])[0] - xm180;
+            slope = vert/(horz1 + horz2);
+            path+= "L" + x180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
+            path+= "M" + xm180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
+        } 
+
+        else if (coords[0][0] < -70 & coords[1][0]>105) {
+            horz1 = proj(coords[0])[0] - xm180;
+            horz2 = x180 - proj(coords[1])[0];
+            slope = vert/(horz1 + horz2);
+            path+= "L" + xm180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
+            path+= "M" + x180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
         }
-        else{
-          console.log("Path already highlighted");
-          routeLine.remove();
-          d3.select(this)
-            .attr("fill","#000000")
-            .attr("stroke", "#000000");
-        }
-      });
-
-    var ports = mapSvg.append("g");
-
-    ports.selectAll("path")
-      .data(ports_d)
-      .enter()
-      .append("path")
-      .attr("fill", "#900")
-      .attr("stroke", "#999")
-      .attr("d", function(d) {
-        var feature = {
-          "type":"Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [d.long, d.lat]
-          }
-        };
-        return geoPath(feature);
-      })
-      .on("click", function() {
-        d3.select(this)
-          .attr("fill", "#342a99")
-          .attr("stroke", "#342a99");
-      });
-
-  function buildRoute(coords) {
-    // var west = false;
-    // if ()
-
-    // calculate east to west if start >105 and ends up on the west coast 
-    // how to define west coast of americas?(<70 if south america or <)
-
-    var x180 = proj([180,coords[0][1]])[0];
-    var xm180 = proj([-179.999,coords[0][1]])[0];
-    var vert = proj(coords[1])[1] - proj(coords[0])[1]; // y of start minus y of end
-    var horz1;
-    var horz2;
-    var slope;
-
-    var path= "M" + proj(coords[0])[0] + "," + proj(coords[0])[1];
-    if (coords[0][0]>105 & coords[1][0] < -70) {
-      horz1 = x180 - proj(coords[0])[0];
-      horz2 = proj(coords[1])[0] - xm180;
-      slope = vert/(horz1 + horz2);
-      //console.log(coords[0]); //**
-      path+= "L" + x180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
-      path+= "M" + xm180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
-    } 
-
-    else if (coords[0][0] < -70 & coords[1][0]>105) {
-      horz1 = proj(coords[0])[0] - xm180;
-      horz2 = x180 - proj(coords[1])[0];
-      slope = vert/(horz1 + horz2);
-      //console.log(coords[0]); //**
-      path+= "L" + xm180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
-      path+= "M" + x180 + ","; path+= (proj(coords[0])[1] + slope * horz1);
+        path+= "L" + proj(coords[1])[0] + "," + proj(coords[1])[1];
+        return path;
     }
-    path+= "L" + proj(coords[1])[0] + "," + proj(coords[1])[1];
-    return path;
-  }
 
-  // function getFrequencies(data) {
-  //   var currentCompany = current.Company;
-  //   if(!freq.hasOwnProperty(currentCompany)) freq[currentCompany] = 0;
-  //   freq[currentCompany]++;
-  //   return freq;
-  // }
+    function allFilters(ports_d, routes_d, vessels_d, traversals_d) {
+        // filter all this data
+        // produce array of route ids, port ids and frequencies
+        
+        // filter vessels
+        vessels_d = age_filter.filter(vessels_d, d3.select("#type_filter"));
+
+        var vessels = vessels_d.map(function(d) { return d.id; });
+        var routes = routes_d.map(function(d) { return d.routeID; });
+        traversals_d.filter(function(d) {
+            return !(routes.indexOf(d.vesselID) === -1) && !(vessels.indexOf(d.vesselID) === -1);
+        });
+
+        // calculate frequencies by route
+        // TODO
+        return getFrequencies(routes_d, traversals_d);
+
+
+        // var routes = [
+        //     { routeID: "1", startportID: "3", endportID: "2567", frequency: "334" },
+        //     { routeID: "2", startportID: "7", endportID: "289", frequency: "123" },
+        //     { routeID: "3", startportID: "7", endportID: "1602", frequency: "2199" } 
+        // ];
+
+        // return routes;
+    }
+
+    function getFrequencies(routes_d, traversals_d) {
+        // var trav2006 = traversals_d.filter(function(d) {
+        //     return d.year === 2006;
+        // });
+        // var trav2009 = traversals_d.filter(function(d) {
+        //     return d.year === 2009;
+        // });
+        // var trav2012 = traversals_d.filter(function(d) {
+        //     return d.year === 2012;
+        // });
+
+        var routeIDs = []; 
+        var route_data = [];
+
+        var len = traversals_d.length;
+        for (var i=0; i<len; i++) {
+            var ti = traversals_d[i];
+            var index = routeIDs.indexOf(ti.routeID);
+            
+            if (index === -1) {
+                routeIDs.push(ti.routeID);
+                route_data.push({
+                    routeID: ti.routeID,
+                    startportID: ti.startportID,
+                    endportID: ti.endportID,
+                    freq2006: 0,
+                    freq2009: 0,
+                    freq2012: 0
+                });
+            };
+
+            index = routeIDs.indexOf(ti.routeID);
+            switch(ti.year) {
+                case "2006":
+                    route_data[index].freq2006 = route_data[index].freq2006 + 1;
+                    break;
+                case "2009":
+                    route_data[index].freq2009++;
+                    break;
+                case "2012":
+                    route_data[index].freq2012++;
+                    break;
+            }
+
+        }
+
+        console.log(route_data);
+        return route_data;
+    }
+
+    // function getFrequencies(data) {
+    //   var currentCompany = current.Company;
+    //   if(!freq.hasOwnProperty(currentCompany)) freq[currentCompany] = 0;
+    //   freq[currentCompany]++;
+    //   return freq;
+    // }
 
 }
